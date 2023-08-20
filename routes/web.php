6063cdc9b4313fc4c6740a5d3c333013b40b9943
gvijay824@gmail.com <gvijay824@gmail.com>
2023-08-20 12:04:49 +0530

    added

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,23 @@
 <?php
-
+  
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CustomAuthController;
-
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-*/
-Route::get('/', [CustomAuthController::class, 'dashboard']); 
-Route::get('dashboard', [CustomAuthController::class, 'dashboard']); 
-Route::get('login', [CustomAuthController::class, 'index'])->name('login');
-Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom'); 
-Route::get('registration', [CustomAuthController::class, 'registration'])->name('register-user');
-Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom'); 
-Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout'); 
+  
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
+// use App\Http\Controllers\ProductController;
+  
+  
+Route::get('/', function () {
+    return view('welcome');
+});
+  
+Auth::routes();
+  
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+  
+Route::group(['middleware' => ['auth']], function() {
+    Route::resource('roles', RoleController::class);
+    Route::resource('users', UserController::class);
+    // Route::resource('products', ProductController::class);
+});
